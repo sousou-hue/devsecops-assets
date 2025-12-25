996ec3d945386ac680514dda2180f4190d758bef
ccn2 <ccn2@ucd.ac.ma>
2025-12-25 19:50:03 +0100

    Secret Détecté

---
+++ key.py
@@ -0,0 +1,13 @@
+
+# Fausse configuration AWS pour test de sécurité
+aws_access_key_id = "AKIAIMNOTREALKEY1234"
+aws_secret_access_key = "WaImNotRealSecretKeyDontUseMeInProd12345"
+
+# Fausse clé privée (C'est ça qui va déclencher l'alarme à 100%)
+private_key = """
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA3Tz2mr7SZiAMfQyuvBjM9OiFj/3e4gI1Nr4wK7Z9
+wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+s8f7d9s8d7f9s8d7f9s8d7f9s8d7f9s8d7f9s8d7f9s8d7f9s8d7f9s8
+-----END RSA PRIVATE KEY-----
+"""
